8c4106ac4de05a03a35929c04de8aeb5fe9a0ac8
Jack Kelly <jack-list@xlk.org.uk>
2015-01-08 18:00:22 +0000

    happy with mains power histogram

--- mains_power_histograms.py
+++ mains_power_histograms.py
@@ -8,14 +8,16 @@ import pytz
 from os.path import join
 
 from pylab import rcParams
-rcParams.update({'figure.figsize': plot_config._mm_to_inches(180, 140)})
+rcParams.update({'figure.figsize': plot_config._mm_to_inches(88, 60)})
 
 print("plotting histograms...")
 
 dataset = DataSet('/data/mine/vadeec/merged/ukdale.h5')
-dataset.set_window("2013-04-01", None)
+#dataset.set_window("2013-04-01", "2013-05-01")
+dataset.set_window(None, None)
 
-axes = dataset.plot_mains_power_histograms(bins=300, range=(0, 2600))
+axes = dataset.plot_mains_power_histograms(bins=500, range=(5, 500), 
+                                           plot_kwargs={'color': plot_config.BLUE})
 
 for i, ax in enumerate(axes):
     ax.grid(False)
@@ -24,12 +26,12 @@ for i, ax in enumerate(axes):
     plot_config.format_axes(ax, tick_size=2)
     sns.despine(ax=ax, left=True)
     ax.spines['bottom'].set_linewidth(0.2)    
-    ax.set_title('House {}'.format(i+1), y=.4)
+    ax.set_title('House {}'.format(i+1), y=.5, va='top', x=0.08)
     if i != 4:
         ax.set_xlabel('')
 
 plt.tight_layout()
-#plt.subplots_adjust(hspace=0.2)
+plt.subplots_adjust(hspace=0.1)
 
 plt.draw()
 
--- plot_config.py
+++ plot_config.py
@@ -30,3 +30,4 @@ def format_axes(ax, tick_size=4):
 FIG_DIR = expanduser("~/Dropbox/MyWork/imperial/PhD/writing/papers/scientific-data-2014/figures")
 
 BLUE = (0.2980392156862745, 0.4470588235294118, 0.6901960784313725)
+GREEN = (0.3333333333333333, 0.6588235294117647, 0.40784313725490196)
