6eac4ced4a944e1f67a5d469f288cfe8908703e4
Jack Kelly <jack-list@xlk.org.uk>
2015-01-08 22:09:54 +0000

    happy with the appliance power histograms

--- appliance_power_histograms.py
+++ appliance_power_histograms.py
@@ -9,23 +9,31 @@ import pytz
 from os.path import join
 
 from pylab import rcParams
-rcParams.update({'figure.figsize': plot_config._mm_to_inches(180, 200)})
+rcParams.update({'figure.figsize': plot_config._mm_to_inches(180, 100)})
 
 print("plotting appliance power histograms...")
 
 dataset = DataSet('/data/mine/vadeec/merged/ukdale.h5')
-dataset.set_window("2013-04-01", None)#"2013-05-02") 
+dataset.set_window("2013-04-26", None) # ignore tungsten kitchen lamps
 elec = dataset.buildings[1].elec
 
 fig, axes = plt.subplots(3, 3)
 meter_keys = ['fridge freezer', 'kettle', 'toaster', 
               'vacuum cleaner', 'television', 'oven',
               'laptop computer', 'computer monitor', ('light', 1)]
-kwargs_per_meter = {'range': [(  10,  110), (2200, 2460), (1480, 1650), 
+kwargs_per_meter = {'range': [(  2,  275), (2200, 2460), (1480, 1650), 
                               ( 400, 2200), (  80,  140), (None,   60),
-                              (   5,   60), (  30,   90), None]}
+                              (   2,   65), (  30,   85), (35, 290)]}
+
 axes = elec.plot_multiple(axes, meter_keys, 'plot_power_histogram', 
-                          kwargs_per_meter, bins=40)
+                          kwargs_per_meter, 
+                          plot_kwargs={'color': plot_config.BLUE})
+
+new_names = {
+    'Light': 'Dimmable LED kitchen ceiling lights',
+    'Computer monitor': '24\" LCD computer monitor',
+    'Television': '34\" CRT television'
+}
 
 # Formatting
 for i, ax in enumerate(axes):
@@ -35,15 +43,20 @@ for i, ax in enumerate(axes):
     plot_config.format_axes(ax, tick_size=2)
     ax.xaxis.set_ticks_position('bottom')
     ax.xaxis.set_major_locator(MaxNLocator(4))
-    for spine in ['right', 'top', 'left']:
-        ax.spines[spine].set_visible(False)
+    for spine in ax.spines.values():
+        spine.set_visible(False)
     if i == 7:
         ax.set_xlabel('Power (watts)')
     else:
         ax.set_xlabel('')
+    
+    title = ax.get_title()
+    if title in new_names:
+        title = new_names[title]
+    ax.set_title(title, y=0.95)
 
 plt.tight_layout()
-plt.subplots_adjust(hspace=0.3)
+plt.subplots_adjust(hspace=1.0)
 
 plt.draw()
 plt.savefig(join(plot_config.FIG_DIR, '08_appliance_power_histograms.eps'), 
