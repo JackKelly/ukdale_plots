d6db3e6afa4058729d53edb7c66b6ec53cbc35f1
Jack Kelly <jack-list@xlk.org.uk>
2015-01-09 12:27:00 +0000

    Use 'Home theatre PC' instead of 'HTPC'.  And print the kWh per appliance.

--- plot_top_5_energy.py
+++ plot_top_5_energy.py
@@ -9,7 +9,6 @@ from os.path import join
 import pandas as pd
 
 from pylab import rcParams
-rcParams.update({'figure.figsize': plot_config._mm_to_inches(60, 90)})
 
 print("plotting energy bar...")
 
@@ -20,18 +19,25 @@ elec = dataset.buildings[1].elec
 submeters = elec.meters_directly_downstream_of_mains()
 grouped = submeters.groupby('type')
 top_k = grouped.select_top_k(group_remainder=False)
+try:
+    top_k['HTPC'].name = "Home theatre PC"
+except KeyError:
+    pass
 energy = top_k.energy_per_meter(mains=elec.mains(), per_period='D',
                                 use_meter_labels=True)
 
 energy.sort(ascending=False)
 
+# Plot
+rcParams.update({'figure.figsize': plot_config._mm_to_inches(70, 90)})
 ax = pd.DataFrame(energy).T.plot(kind='bar', stacked=True, grid=True,
                                  edgecolor="none", legend=False, width=2)
 ax.set_xticks([])
-ax.set_ylabel('kWh', rotation=0, ha='right', va='center', labelpad=15)
+ax.set_ylabel('kWh\nper\nday', rotation=0, ha='center', va='center', labelpad=15)
 
 text_ys = energy.cumsum() - energy.cumsum().diff().fillna(energy['Remainder']) / 2
-for label, y in text_ys.iteritems():
+for kwh, (label, y) in zip(energy.values, text_ys.iteritems()):
+    label += " ({:.2f})".format(kwh)
     ax.annotate(label, (0, y), 
                 horizontalalignment='center', verticalalignment='center', 
                 color='white', size=8)
