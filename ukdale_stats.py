db16b468f5ef1347ceae21b974fc34964b23d54c
Jack Kelly <jack-list@xlk.org.uk>
2015-01-12 15:16:21 +0000

    adding meter comparisons and tidying up code in ukdale_stats.py

--- ukdale_stats.py
+++ ukdale_stats.py
@@ -1,28 +1,19 @@
 from __future__ import print_function, division
-from nilmtk import DataSet, TimeFrame, MeterGroup
+from nilmtk import DataSet
 
 dataset = DataSet('/data/mine/vadeec/merged/ukdale.h5')
-TZ = dataset.metadata['timezone']
-elec = dataset.buildings[1].elec
-
-for building in dataset.buildings.values():
-    try:
-        building.elec.use_alternative_mains()
-    except RuntimeError:
-        pass
-    else:
-        print("using alternative mains for", building)
 
 window_per_house = {1: ("2013-04-12", None), 
                     2: ("2013-05-22", None), 
                     3: (None, None), 
                     4: (None, None), 
                     5: (None, "2014-09-06")}
+
 descriptions = []
-for house in range(2,6):
-    print("*********** House", house, "*************")
-    dataset.set_window(*window_per_house[house])
-    description = dataset.buildings[house].describe()
+for building_id, building in dataset.buildings.iteritems():
+    print("*********** House", building_id, "*************")
+    dataset.set_window(*window_per_house[building_id])
+    description = building.describe()
     descriptions.append(description)
     print(description)
     print()
